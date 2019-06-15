34b9d2172dfc3cef9f761d76545174df83c26d3d
Mathew Grabau <mgrabau@mathewgrabau.ca>
2019-06-15 01:18:20 -0500

    Added data service (testing/scratch first)

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,17 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            // TODO need the loading from database.
+            yield return new Friend { FirstName = "Mathew", LastName = "Grabau" };
+            yield return new Friend { FirstName = "Charlie", LastName = "Brown" };
+            yield return new Friend { FirstName = "Ned", LastName = "Flanders" };
+            yield return new Friend { FirstName = "Hank", LastName = "Hill" };
+        }
+    }
+}
