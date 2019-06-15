faa0354460dade60b4a6301767d3183efdb86295
Mathew Grabau <mgrabau@mathewgrabau.ca>
2019-06-15 01:12:03 -0500

    Added basic model required for the application.

--- FriendOrganizer.Model/Friend.cs
+++ FriendOrganizer.Model/Friend.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FriendOrganizer.Model
+﻿namespace FriendOrganizer.Model
 {
     public class Friend
     {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
     }
 }
