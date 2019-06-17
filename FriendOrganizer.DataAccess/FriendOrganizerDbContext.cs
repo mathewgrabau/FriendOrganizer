42fd8fb0113aae20905442cd74e2233e51fdbfba
Mathew Grabau <mgrabau@mathewgrabau.ca>
2019-06-16 22:37:05 -0500

    Begin creating the db context.

---
+++ FriendOrganizer.DataAccess/FriendOrganizerDbContext.cs
@@ -0,0 +1,24 @@
+﻿using FriendOrganizer.Model;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace FriendOrganizer.DataAccess
+{
+    public class FriendOrganizerDbContext : DbContext
+    {
+        public FriendOrganizerDbContext() : base("FriendOrganizerDb")
+        {
+
+        }
+
+        public DbSet<Friend> Friends { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Customize naming convention (singular form of table naming).
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
